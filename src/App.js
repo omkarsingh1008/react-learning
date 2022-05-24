a45123460c0807a9f50ba1643458bceb843c8f1c
omkarsingh1008 <omkarsinghrajpurohit363@gmail.com>
2022-05-24 15:04:11 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: 'omkar',
+    }; 
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello {this.state.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button 
+        onClick={() => {
+          this.setState({ name: 'ok' })
+        }}
         >
-          Learn React
-        </a>
+        change name
+        </button>
+        <p>hh</p>
       </header>
     </div>
   );
 }
-
+}
 export default App;
